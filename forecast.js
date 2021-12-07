088e279401178c227323c2eb7627c1af863726a6
Mustafa Sh <81806867+Mustafa681@users.noreply.github.com>
2021-12-07 16:48:45 +0100

    Update forecast.js

--- forecast.js
+++ forecast.js
@@ -2,10 +2,10 @@ class Forecast {
   constructor() {
     this.key = 'bkMZ5EXxNrIrmV6y2887HZlRLITpMJno'
 
-    this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    this.weatherURI = 'https://dataservice.accuweather.com/currentconditions/v1/'
 
     this.cityURI =
-      'http://dataservice.accuweather.com/locations/v1/cities/search'
+      'https://dataservice.accuweather.com/locations/v1/cities/search'
   }
   async updateCity(city) {
     const cityDets = await this.getCity(city)
